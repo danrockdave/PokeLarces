9fac426a17bf21ef23fb81b4887ec48a46455deb
danrockdave <davifontenele_@hotmail.com>
2024-01-04 16:53:41 -0300

    151 pokemons na tela, com nome e imagem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,44 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import PokemonCard from '../components/PokemonCard'
+import { Container, Grid } from '@mui/material'
+import axios from "axios";
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    const [pokemons, setPokemons] = useState([]);
+
+    useEffect(() => {
+        getPokemons();
+    }, []);
+
+    const getPokemons = () => {
+        var endpoints = []
+        for (var i = 1; i < 151; i++) {
+            endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        }
+        axios.all(
+            endpoints.map((endpoint) =>
+                axios
+                    .get(endpoint)))
+                    .then((res) => setPokemons(res))
+                    .catch((error) => console.log(error));
+    }
+
+    return (
+        <div>
+            <Navbar />
+            <Container maxWidth="false">
+                <Grid container spacing={3}>
+                    {pokemons.map((pokemon, key) => (
+                        <Grid item xs={2} key={key}>
+                            <PokemonCard name={pokemon.data.name} image={pokemon.data.sprites.front_default} />
+                        </Grid>
+                    ))}
+                </Grid>
+
+            </Container>
+        </div>
+    )
 }
